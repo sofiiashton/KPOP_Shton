3adfb24cfd8671ef4eae7e698f9599c82601cf56
sofiiashton <shtonsofia23@gmail.com>
2022-12-03 00:08:57 -0500

    Added enum

--- Lab1/dist/Lab1.js
+++ Lab1/dist/Lab1.js
@@ -1,16 +1,25 @@
 "use strict";
+var Category;
+(function (Category) {
+    Category[Category["BusinessAnalyst"] = 0] = "BusinessAnalyst";
+    Category[Category["Developer"] = 1] = "Developer";
+    Category[Category["Designer"] = 2] = "Designer";
+    Category[Category["QA"] = 3] = "QA";
+    Category[Category["ScrumMaster"] = 4] = "ScrumMaster";
+})(Category || (Category = {}));
+;
 function getAllworkers() {
     let workers = [
-        { Name: 'Ivan', surname: 'Ivanov', available: true, salary: 1000 },
-        { Name: 'Petro', surname: 'Petrov', available: true, salary: 1500 },
-        { Name: 'Vasyl', surname: 'Vasyliev', available: false, salary: 1600 },
-        { Name: 'Evgen', surname: 'Zhukov', available: true, salary: 1300 }
+        { Name: 'Ivan', surname: 'Ivanov', available: true, salary: 1000, Category: Category.QA },
+        { Name: 'Petro', surname: 'Petrov', available: true, salary: 1500, Category: Category.BusinessAnalyst },
+        { Name: 'Vasyl', surname: 'Vasyliev', available: false, salary: 1600, Category: Category.Developer },
+        { Name: 'Evgen', surname: 'Zhukov', available: true, salary: 1300, Category: Category.Designer }
     ];
     return workers;
 }
 function logFirstAvailable(arr) {
-    console.log(`Кількість робітників: ${arr.length}`);
-    console.log(`Доступні робітники:`);
+    console.log(`\nКількість робітників: ${arr.length}`);
+    console.log(`\nДоступні робітники:`);
     for (var w of arr) {
         if (w.available == true) {
             console.log(w.Name, w.surname);
@@ -18,4 +27,29 @@ function logFirstAvailable(arr) {
     }
 }
 logFirstAvailable(getAllworkers());
+function getWorkersNamesByCategory(cat) {
+    let surnames = [];
+    for (var w of getAllworkers()) {
+        if (w.Category == cat) {
+            surnames.push(w.surname);
+        }
+    }
+    if (surnames.length == 0) {
+        return undefined;
+    }
+    else {
+        return surnames;
+    }
+}
+let myCategory = Category.Designer;
+if (typeof getWorkersNamesByCategory(myCategory) === 'undefined') {
+    console.log(`\nУ масиві немає робітників, які належать категорії ${myCategory}.`);
+}
+else {
+    console.log(`\nПрізвища робітників, які належать категорії ${Category[myCategory]}:`);
+    var mySurnames = getWorkersNamesByCategory(myCategory);
+    for (var i in mySurnames) {
+        console.log(mySurnames[i]);
+    }
+}
 //# sourceMappingURL=Lab1.js.map
--- Lab1/src/Lab1.ts
+++ Lab1/src/Lab1.ts
@@ -1,9 +1,17 @@
+enum Category { 
+    BusinessAnalyst, 
+    Developer, 
+    Designer, 
+    QA, 
+    ScrumMaster};
+
+// 1. Базові типи
 function getAllworkers() {
     let workers = [
-    {Name: 'Ivan', surname: 'Ivanov', available: true, salary: 1000},
-    {Name: 'Petro', surname: 'Petrov', available: true, salary: 1500},
-    {Name: 'Vasyl', surname: 'Vasyliev', available: false, salary: 1600},
-    {Name: 'Evgen', surname: 'Zhukov', available: true, salary: 1300}
+    {Name: 'Ivan', surname: 'Ivanov', available: true, salary: 1000, Category: Category.QA},
+    {Name: 'Petro', surname: 'Petrov', available: true, salary: 1500, Category: Category.BusinessAnalyst},
+    {Name: 'Vasyl', surname: 'Vasyliev', available: false, salary: 1600, Category: Category.Developer},
+    {Name: 'Evgen', surname: 'Zhukov', available: true, salary: 1300, Category: Category.Designer}
     ]
     return workers;
     }
@@ -11,18 +19,50 @@ function getAllworkers() {
 function logFirstAvailable(arr:any[]) {
     
     // кількість робітників
-    console.log(`Кількість робітників: ${arr.length}`)
+    console.log(`\nКількість робітників: ${arr.length}`)
 
     // імя та прізвище доступного робітника
-    console.log(`Доступні робітники:`)
+    console.log(`\nДоступні робітники:`)
     
     for(var w of arr) {
         if(w.available==true) {
             console.log(w.Name, w.surname)
         }
     }
-
-
 }
 
-logFirstAvailable(getAllworkers())
+logFirstAvailable(getAllworkers());
+
+// 2. Enum
+function getWorkersNamesByCategory(cat:Category) {
+
+    let surnames:string[] = [];
+
+    for(var w of getAllworkers()) {
+        if(w.Category==cat) {
+            surnames.push(w.surname)
+        }
+    }
+
+    if(surnames.length==0) {
+        // empty array
+        return undefined
+    } else {
+        return surnames
+    }
+}
+
+let myCategory:Category = Category.Designer;
+
+if(typeof getWorkersNamesByCategory(myCategory) === 'undefined') {
+    console.log(`\nУ масиві немає робітників, які належать категорії ${myCategory}.`)
+} else {
+    console.log(`\nПрізвища робітників, які належать категорії ${Category[myCategory]}:`)
+
+    var mySurnames:string[] = getWorkersNamesByCategory(myCategory)!
+    for(var i in mySurnames) {
+        console.log(mySurnames[i])
+    }
+}
+
+// 3. Стрілочні функції
