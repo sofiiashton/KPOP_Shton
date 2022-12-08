bdb503dd906a62f4018a829029dc7d53df364787
sofiiashton <shtonsofia23@gmail.com>
2022-12-08 17:12:15 -0500

    Draft

--- lab5/lab5/src/app/app.component.ts
+++ lab5/lab5/src/app/app.component.ts
@@ -3,23 +3,13 @@ import { AfterViewInit, Component, Inject, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Post } from './web.service';
-import { AnimateTimings } from '@angular/animations';
+import { MatButtonModule } from '@angular/material/button'; 
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
-
-import { ExampleDialogComponent } from './example-dialog.component';
-
 @Component ({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  animations: [
-    trigger('detailExpand', [
-      state('collapsed', style({ height: '0px', minHeight: '0', visibility: 'hidden' })),
-      state('expanded', style({ height: '*', visibility: 'visible' })),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ]),
-  ]
+  styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
@@ -27,13 +17,13 @@ export class AppComponent {
   title = 'lab5';
 
   listPosts:Post[] = []
-  displayedColumns: string[] = ['userId', 'id', 'title', 'body'];
+  displayedColumns: string[] = ['userId', 'id', 'title', 'body', 'actions'];
 
   dataSource: any
 
 @ViewChild(MatPaginator) paginator: MatPaginator;
 
-constructor(private web:WebService) {
+constructor(private web:WebService, private dialog: MatDialog) {
   this.web.getPost().subscribe(data => {
     this.listPosts = data;
     this.dataSource = new MatTableDataSource(this.listPosts)
@@ -42,6 +32,36 @@ constructor(private web:WebService) {
   })
 }
 
-clickedRows = new Set<Post>();
+openDialog(row) {
+  this.dialog.open(ExampleDialog, {
+    width: '500px',
+    data: row,
+  })
+}
+}
 
+@Component({
+  selector: "example-dialog",
+  // templateUrl: "example-dialog.component.html"
+  template: `
+            <div>userId: {{ngOnInit().userId}}</div>
+            <div>Id: {{ngOnInit().id}}</div>
+            <div>Title: {{ngOnInit().title}}</div>
+            <div>Body: {{ngOnInit().body}}</div>`,
+            
+  styleUrls: ['./example-dialog.component.css']
+})
+export class ExampleDialog {
+  constructor(
+    private dialogRef: MatDialogRef<ExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) private data: any)
+  { }
+
+  ngOnInit() {
+    return this.data
+  }
+
+  onCloseClick(): void {
+    this.dialogRef.close();
+  }
 }
--- lab5/lab5/src/app/app.module.ts
+++ lab5/lab5/src/app/app.module.ts
@@ -1,28 +1,34 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ExampleDialogModule } from './example-dialog.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatDialogModule } from '@angular/material/dialog';
+import { ChildComponent } from './child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent
   ],
   imports: [
-    ExampleDialogModule,
     BrowserModule,
     MatTableModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatButtonModule,
+    MatDialogModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [AppComponent, ChildComponent]
+
 })
 export class AppModule { }
